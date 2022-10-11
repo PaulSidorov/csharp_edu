addb17440fd0f1e79c5e97e0f5383b8671839f18
Сидоров Павел <izh306@list.ru>
2022-10-11 14:43:34 +0500

    задание 3. Вспомогательные классы

---
+++ Address_book_PS/Address_book_PS/AccountData.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Address_book_PS
+{
+    internal class AccountData1
+    {
+        private string username;
+        private string password;
+
+        public AccountData1(string username, string password)
+        {
+            this.username = username;
+            this.password = password;
+        }
+
+        public string Username
+            { get { return username; } set { username = value; } }
+
+        public string Password
+            { get { return password; } set { password = value; } }
+
+
+    }
+
+
+
+    internal class AccountData
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public AccountData(string username, string password)
+        {
+            this.Username = username;
+            this.Password = password;
+        }
+
+        
+
+
+    }
+
+
+
+}
--- Address_book_PS/Address_book_PS/UnitTest1.cs
+++ Address_book_PS/Address_book_PS/UnitTest1.cs
@@ -1,3 +1,5 @@
+using NUnit.Framework;
+
 namespace Address_book_PS
 {
     public class Tests
