d177633c580f8f0b40e5288b95b0339a3196da87
Сидоров Павел <izh306@list.ru>
2022-10-12 11:00:04 +0500

    Задание 4. добавление контакта

---
+++ Address_book_PS/Address_book_PS/ContactData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Address_book_PS
+{
+    internal class ContactData
+    {
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public string MiddleName { get; set; } = "";
+        public string NickName { get; set; } = "";
+        public string Title { get; set; } = "";
+        public string Company { get; set; } = "";
+        public string Address { get; set; } = "";
+        public string TelHome { get; set; } = "";    
+        public string TelMobile { get; set; } = "";
+        public string TelWork { get; set; } = "";
+        public string TelFax { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string Email2 { get; set; } = "";
+        public string Email3  { get; set; } = "";
+        public string Homepage { get; set; } = "";
+        public string BirthDay { get; set; } = "";
+        public string BirthMonth  { get; set; } = "";
+        public string BirthYear { get; set; } = "";
+        public string AnniversaryDay { get; set; } = "";
+        public string AnniversaryMonth { get; set; } = "";
+        public string AnniversaryYear { get; set; } = "";
+        public string SecondaryAddress { get; set; } = "";
+        public string SecondaryTelHome { get; set; } = "";
+        public string Notes { get; set; } = "";
+
+
+
+}
+}
